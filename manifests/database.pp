ffc54b7177609b03a442ece02fb220e1ba5162d9
Jason Cox <j_cox@globelock.dyndns.info>
2014-06-08 16:00:06 +1000

    add the user to the db creation

--- manifests/database.pp
+++ manifests/database.pp
@@ -44,7 +44,7 @@ define postgresplus::database(
     default => "--tablespace='${tablespace}' ",
   }
 
-  $createdb_command = "${createdb_path} --owner='${owner}' --template=${template} ${encoding_option}${locale_option}${tablespace_option} '${dbname}'"
+  $createdb_command = "${createdb_path} -U ${owner} --owner='${owner}' --template=${template} ${encoding_option}${locale_option}${tablespace_option} '${dbname}'"
 
   postgresql_psql { "Check for existence of db '${dbname}'":
     command => 'SELECT 1',
